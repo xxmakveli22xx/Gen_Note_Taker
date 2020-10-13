7ec4f0a678a9dd00f8f6b6867b80e84197e52c26
Pete Hernandez <pete.g.hernandez@gmail.com>
2020-10-12 22:03:00 -0500

    created the link

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.use(express.static("public"));
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
 
 app.get("/",(req,res) => {
-    res.sendFile(path.join(__dirname,"../public/index.html"));
+    res.sendFile(path.join(__dirname,"./public/index.html"));
   });
 
 app.get("/notes",(req,res) => {
-    res.sendFile(path.join(__dirname,"../public/notes.html"));
+    res.sendFile(path.join(__dirname,"./public/notes.html"));
   });
 
 app.get("/api/notes",(req,res) => {
